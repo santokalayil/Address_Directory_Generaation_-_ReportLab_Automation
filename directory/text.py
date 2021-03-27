45276b627a8252ca18822c92a9e7a4a114649c1d
Santo K Thomas <49450970+santokalayil@users.noreply.github.com>
2021-03-27 18:07:51 +0530

    committing files

---
+++ directory/text.py
@@ -0,0 +1,83 @@
+data = {
+    'title': dict(
+        full_name = 'Santo K Thomas',
+        job_title='''Data Scientist''',
+    ),
+    'summary': dict(
+        heading = 'Summary',
+        text='''Highly analytical and process-oriented data scientist, with in depth knowledge of database types, research methodologies, highly adept at accurate curation, analysis, and visualization of data.  Possess extensive analytical skills, strong attention to detail, team player, resilient with a strong work ethic to succeed. Dream to initialize or to be part of at least one innovation in the field of Data Science for this millennium.''',
+        links = [
+            '''Github:\thttps://github.com/santokalayil''',
+            '''LinkedIn:\thttps://www.linkedin.com/in/santokalayil''',
+            '''Kaggle:\thttps://www.kaggle.com/santokalayil''',
+        ], 
+    ),
+    'keyskills': dict(
+        heading="Key Skills",
+        skills=[
+            '''Data Analysis & Visualization: using Python, Pandas, Numpy, Matplotlib, Seaborn, Plotly, Plotly Dash,Tableu''',
+            '''Machine Learning: Supervised & Unsupervised Learning (Regression, Classification, PCA, Clustering.)''',
+            '''Programming - Python (Advanced), Rust (Basic), Julia(Basic). Python based-skills in automated pdf report and document generation using Report Lab Python and Social Media Automation. GUI App Creation using FLTK Rust, Kivy Python''',
+            '''Database: MySQL, Postgres, Sqlite, MongoDB''',
+            '''Operating Systems: Linux (Bash & Python Scripting), Windows. Command Line Skills (Linux terminal, Windows Command Prompt and Powershell)''',
+            '''Data Collection using Web-scraping and Web-automation tools like Beautiful Soup, Selenium, Requests, PyAutoGUI''',
+            '''Statistics, Problem Solving''',
+            '''Web Development and other tools (HTML5, CSS, Bootstrap 4, Tailwind CSS, Django, Flask, Google Forms, Google sheet API)''',
+            '''Writing Projects and Reports using Adobe InDesign, Affinity Publisher, Microsoft Office, etc.''',
+            '''Coordinate, communicate and provide technical support to other functional groups relating to web applications.''',
+        ]
+    ),
+    'professional_summary': dict(
+        heading="Professional Summary",
+        experiences=[
+            '''Website Developer & Administrator at Sacred Heart Matriculation School, Padi, Chennai (until 2019)''',
+            '''Freelance: Web Developer & Administrator for Diocese of Pathanamthitta, Carmal Central School, Carmala, Pathanamthitta, Kerala''',
+            '''Letters and Reports writing in Bishop's Office, Pathanamthitta (Writing Projects, Reports.)''',
+            '''Freelance: Data Collection and Automation, Analytics and Directory DTP Book Generation for Hennur MCC, Bangalore''',
+        ]
+    ),
+    'projects': dict(
+        heading="Selected Projects",
+        projects=[
+            '''Capstone Project: Cancer (Biopsy) prediction with high accuracy though the dataset is based on patients' survey. Used most of the classification methods and attained highest accuracy using Logistic Regression by simplifying the complexity of the dataset.''',
+            '''Heart Desease Prediction Project: Classification problem which predicted the heart decease patients with 99% Sensitivity and 97% Accuracy.''',
+            '''Data Analytics Project on Price Analysis and Linear Regression on Melbourne Housing Dataset.''',
+            '''Employee Attrition & Performance Analytics based on IBM Employee Dataset - 60% more faster detection of factors that determine and influence employee performance and attrition so that the HR department can introduce new cost effective strategies to improve productivity and performance.''',
+            '''Customer Analytics Project: Exploratory Data Analysis, Predicting Customer Satisfaction based on a Brazilian Public E-commerce Data.''',
+            # https://www.kaggle.com/andresionek/predicting-customer-satisfaction
+            '''Bank Personal Loan Modelling Data Science Project - Designed an optimal and efficient model that could help the bank identify the potential customers who have a higher probability of purchasing the loan which will increase the success ratio while at the same time reduce the cost of the campaign. The built ML Models attained upto 96% Specificity.''',
+            '''Automated Directory Project: Freelance project undertaken for St. Mary's Community, Hennur - which automated most of the steps like data collection, database storage, data retrieval and analytics, rearranging data for various sections of the book, data correction, Image resizing, full PDF generation etc. Technologies and Tools used are Google Forms, Google Sheet API, Python (Pandas, Numpy, Flask, Matplotlib, Reportlab, Pillow, OpenCV, Sqlite etc.''',
+            '''Current Personal Project: Developing a multipurpose NGO software for data collection, retrieval, analytics, automated certificate, report and letter generation. Support for desktop, mobile and web platforms. Techologies used are Python(Kivy, Tkinter, Flask, PyMongo, Reportlab, etc.), Rust(FLTK).''',
+        ]
+    ),
+    'education': dict(
+        heading="Education",
+        table= {
+            'columns': ['Course', 'Institution', 'Year', 'Remarks'],
+            'data': [
+                ['Postgraduate Programme in \nData Science and Engineering','Great Lakes Institute of Management','2020','Certificate Link: \nhttps://verify.greatlearning.in/FFEJOUXA'],
+                ['B Th.', 'Urbaniana University, ROMA', '2014', '85%'],
+                ['B A-Philosophy', 'University of Kerala', '2010', '62%'],
+                ['Higher Secondary','St. Mary’s Higher Secondary School, Pattom, Trivandrum','2007','82%'],
+                ['SSLC-10','Sacred Heart High School, Mylapra, Pathanamthitta','2004','80%'],
+            ]
+        }
+    ),
+    'other_certifications': dict(
+        heading="Other Certifications & Achievements",
+        certificates=[
+            '''ABRSM- Royal School of Music, London - Certified in Music Theory''',
+            '''Author of MALLU python package for Easier Machine Learning Workflow- published under MIT License in The Python Package Index (PyPI) Software Repository. Available via "pip install"''',
+        ]
+    ),
+
+    'contact': dict(
+        name='Santo K Thomas',
+        address="Hennur, Bangalore",
+        email="santokalayil@gmail.com",
+        phone="+91 8891960880",
+        website="www.santokalayil.com",
+    )
+    
+}
+
