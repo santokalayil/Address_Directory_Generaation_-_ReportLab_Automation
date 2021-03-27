1aa078d76c330c27b12cb076ba4b5508b451ff9e
Santo K Thomas <49450970+santokalayil@users.noreply.github.com>
2021-03-27 18:06:29 +0530

    committing files

---
+++ directory/fonts.py
@@ -0,0 +1,14 @@
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.lib.fonts import addMapping
+from reportlab.pdfbase import pdfmetrics
+
+
+# register a new font
+def register():
+    pdfmetrics.registerFont(TTFont('merriweather','fonts/merriweather/Merriweather-Black.ttf'))
+    pdfmetrics.registerFont(TTFont('yanone','fonts/yanone/YanoneKaffeesatz-Light.ttf'))
+    pdfmetrics.registerFont(TTFont('oswald_regular', 'fonts/oswald/Oswald-Regular.ttf'))
+    pdfmetrics.registerFont(TTFont('oswald_semibold', 'fonts/oswald/Oswald-SemiBold.ttf'))
+    pdfmetrics.registerFont(TTFont('oswald_medium', 'fonts/oswald/Oswald-Medium.ttf'))
+    pdfmetrics.registerFont(TTFont('oswald_light', 'fonts/oswald/Oswald-Light.ttf'))
+    pdfmetrics.registerFont(TTFont('oswald_extralight', 'fonts/oswald/Oswald-ExtraLight.ttf'))
