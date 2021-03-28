9cbd36b66897368eea19fcb8808e04daa0647ee2
Santo K Thomas <49450970+santokalayil@users.noreply.github.com>
2021-03-28 22:45:58 +0530

    committing files

--- directory/settings.py
+++ directory/settings.py
@@ -7,7 +7,7 @@ page_size = (PAGE_WIDTH, PAGE_HEIGHT)
 TOP_MARGIN, BOTTOM_MARGIN = 0.5*inch, 0.5*inch
 LEFT_MARGIN, RIGHT_MARGIN = 0.5*inch, 0.5*inch
 
-export_name_url = 'directory.pdf'
+export_name_url = 'directory_final.pdf'
 
 PAGE_FRAME_WIDTH = PAGE_WIDTH - (LEFT_MARGIN + RIGHT_MARGIN)
 PAGE_FRAME_HEIGHT = PAGE_HEIGHT - (TOP_MARGIN + BOTTOM_MARGIN)
