21100b2edd506ec52583bc51388c8c39e18f75df
Santo K Thomas <49450970+santokalayil@users.noreply.github.com>
2021-03-29 17:01:10 +0530

    committing files

--- directory/contents.py
+++ directory/contents.py
@@ -10,14 +10,15 @@ from reportlab.platypus import Paragraph, NextPageTemplate, FrameBreak
 Elements = []
 Elements.append(NextPageTemplate(section_ids.dual_row_family_id))  # marking section id to identify master page and style
 
-from .pages.family_pages.family_box import (
-    family_title_section,
-    photo_and_address_section,
-    members_table_section,
-)
+# from .pages.family_pages.family_box import (
+#     family_title_section,
+#     photo_and_address_section,
+#     members_table_section,
+# )
+from .pages.family_pages.family_box import family_title_section, photo_and_address_section,members_table_section
 
 Elements += [
     family_title_section(fam_id=12, family_head="Thomas K J"),
     photo_and_address_section(),
-    members_table_section()
+    members_table_section(12)
 ]
