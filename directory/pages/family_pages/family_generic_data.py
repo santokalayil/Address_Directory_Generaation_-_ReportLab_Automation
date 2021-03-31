953f9a1aa70d85e168ccdf8cc635a6849b2381b6
Santo K Thomas <49450970+santokalayil@users.noreply.github.com>
2021-03-31 15:54:19 +0530

    committing files

--- directory/pages/family_pages/family_generic_data.py
+++ directory/pages/family_pages/family_generic_data.py
@@ -19,7 +19,7 @@ def family_common_data(fam_id, db_url='sqlite.db'):
         sql(f'''SELECT * FROM cur_addr where famid={fam_id}''').loc[0, 'house_flat_no':'pin'].to_dict().values())
     ca_list = [i for i in ca_list if (i != '') or (i != '')]
     current_address_text = ', '.join([str(i).strip() for i in ca_list])  # we can change later
-    current = f'''Current Address:\n{current_address_text}'''
+    current = f'''{current_address_text}'''
 
     # native address
     na_series = sql(f'''SELECT * FROM nat_addr where famid={fam_id}''').loc[0, 'house_name':'pin']
@@ -28,27 +28,16 @@ def family_common_data(fam_id, db_url='sqlite.db'):
     the_rest = ', '.join([str(i).strip() for i in na_series['place':'pin'].values if len(str(i).strip())])
     na_list = [house_name, po, the_rest]
     native_text = ', '.join(na_list)
-    native = f'''Native Place Address:\n{native_text}''' if len(native_text.strip()) else ''
+    native_address = f'''{native_text}''' if len(native_text.strip()) else ''
 
     # native parish details
     nat_parish_series = sql(f'''SELECT * FROM nat_parish where famid={fam_id}''').loc[0]
-    parish_name = f'''Native Parish: {str(nat_parish_series['name'])}, {str(nat_parish_series['place'])}''' \
+    native_parish = f'''{str(nat_parish_series['name'])}, {str(nat_parish_series['place'])}''' \
         if len(str(nat_parish_series['name']).strip()) else ''
-    diocese = f'''Diocese: {str(nat_parish_series['diocese'])}''' if len(
+    diocese = f'''{str(nat_parish_series['diocese'])}''' if len(
         str(nat_parish_series['diocese']).strip()) else ''
 
-    # para_style = ParagraphStyle(name='Normal', fontName='oswald_light', fontSize=8,
-    #                             spaceAfter=0, spaceBefore=0, leading=10)
-
     current_address = current + '. ' + email_text
-    # current_address = Paragraph(current + '. ' + email_text, style=para_style)
-    # email = Paragraph(email_text, style=para_style)
-    # native_address = Paragraph(native, style=para_style)
-    native_address = native
-    native_parish = parish_name
-    # native_parish = Paragraph(parish_name, style=para_style)
-    diocese = diocese
-    # diocese = Paragraph(diocese, style=para_style)
 
     text = {"current_address": current_address,
             "email": email,
