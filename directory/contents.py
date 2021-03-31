2b45e72e3e5275513027b7e402e9de5ce7f7db54
Santo K Thomas <49450970+santokalayil@users.noreply.github.com>
2021-03-31 15:15:32 +0530

    committing files

--- directory/contents.py
+++ directory/contents.py
@@ -10,15 +10,16 @@ from reportlab.platypus import Paragraph, NextPageTemplate, FrameBreak
 Elements = []
 Elements.append(NextPageTemplate(section_ids.dual_row_family_id))  # marking section id to identify master page and style
 
-# from .pages.family_pages.family_box import (
-#     family_title_section,
-#     photo_and_address_section,
-#     members_table_section,
-# )
 from .pages.family_pages.family_box import family_title_section, photo_and_address_section,members_table_section
 
-Elements += [
-    family_title_section(fam_id=12, family_head="Thomas K J"),
-    photo_and_address_section(),
-    members_table_section(12)
-]
+fam_ids_ordered_alphetically_acc_to_head_of_family = [12,13, 24, 32]
+fam_ids = fam_ids_ordered_alphetically_acc_to_head_of_family
+
+# going to each family data and constructing elements
+for fam_id in fam_ids:
+    Elements += [
+        family_title_section(fam_id=fam_id, family_head="HI TESTING"),
+        photo_and_address_section(fam_id=fam_id),
+        members_table_section(fam_id),
+        FrameBreak()
+    ]
