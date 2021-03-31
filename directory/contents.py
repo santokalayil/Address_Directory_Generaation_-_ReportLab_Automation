fc04b6ee9403292d4358fb395a1e4943b6f20113
Santo K Thomas <49450970+santokalayil@users.noreply.github.com>
2021-03-31 16:16:06 +0530

    committing files

--- directory/contents.py
+++ directory/contents.py
@@ -1,5 +1,6 @@
 from . import section_ids
 from .settings import PAGE_FRAME_WIDTH, PAGE_FRAME_HEIGHT
+from .settings import db_url
 
 from reportlab.lib.styles import ParagraphStyle
 from reportlab.platypus import Paragraph, NextPageTemplate, FrameBreak
@@ -12,10 +13,16 @@ Elements.append(NextPageTemplate(section_ids.dual_row_family_id))  # marking sec
 
 from .pages.family_pages.family_box import family_title_section, photo_and_address_section,members_table_section
 
-fam_ids_ordered_alphetically_acc_to_head_of_family = [12, 13, 24, 32] # 13, 24, 32
-fam_ids = fam_ids_ordered_alphetically_acc_to_head_of_family
+# fam_ids_ordered_alphetically_acc_to_head_of_family = [12, 13, 24, 32] # 13, 24, 32
+# fam_ids = fam_ids_ordered_alphetically_acc_to_head_of_family
 
 # going to each family data and constructing elements
 from .pages.family_pages.family_box import generate_box_elements
-for order_id, fam_id in enumerate(fam_ids, start=1):
+from .database.get import basic_query
+sql = basic_query(db_url).sql_dataframe
+order_of_families = sql(
+    '''select * from members where rltshp = "Self" order by member_name'''
+    ).famid.to_dict().values()
+# print(order_of_families)
+for order_id, fam_id in enumerate(order_of_families, start=1):
     Elements += generate_box_elements(fam_id, order_id)
