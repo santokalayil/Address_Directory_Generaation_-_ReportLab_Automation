a7322fe218e67196972a067ac33c652a466c710e
Santo K Thomas <49450970+santokalayil@users.noreply.github.com>
2021-04-01 11:58:45 +0530

    committing files

--- directory/pages/family_pages/__init__.py
+++ directory/pages/family_pages/__init__.py
@@ -1,5 +1,25 @@
-from .pages.family_pages.family_box import generate_box_elements
-from .database.get import basic_query
+
+
+from directory import master
+from directory import doc
+page_object = master.double_row_frame(doc, ["box_top", "box_bottom"])
+page_frame = page_object.generate()
+
+from reportlab.platypus import PageTemplate
+from directory import section_ids
+page_template = PageTemplate(
+        id="Families", 
+        frames=page_frame,
+        onPage=master.default_master,
+    )
+
+
+from .family_box import generate_box_elements
+from directory.database.get import basic_query
+from directory.settings import db_url
+
+
+
 
 def generate():
     Elements = []
@@ -9,5 +29,5 @@ def generate():
         ).famid.to_dict().values()
 
     for order_id, fam_id in enumerate(order_of_families, start=1):
-    Elements += generate_box_elements(fam_id, order_id)
+        Elements += generate_box_elements(fam_id, order_id)
     return Elements
