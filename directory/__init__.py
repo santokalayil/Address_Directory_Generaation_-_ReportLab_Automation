dc1544ee5e098884f659023cf1b5333f865c5928
Santo K Thomas <49450970+santokalayil@users.noreply.github.com>
2021-04-01 12:17:08 +0530

    committing files

--- directory/__init__.py
+++ directory/__init__.py
@@ -11,17 +11,20 @@ fonts.register()
 doc = master.document()
 
 # setting frame
-# resume_frame = master.single_frame(doc, id='normal')
-families_info_page_object = master.double_row_frame(doc, ["box_top", "box_bottom"])
-families_info_page_frame = families_info_page_object.generate()
-print("BOTTOM FRAME HEIGHT AND WIDTH:", families_info_page_object.bottom_frame.height, families_info_page_object.bottom_frame.width)
-print("TOP FRAME HEIGHT AND WIDTH:", families_info_page_object.top_frame.height, families_info_page_object.top_frame.width)
+
+# families_info_page_object = master.double_row_frame(doc, ["box_top", "box_bottom"])
+# families_info_page_frame = families_info_page_object.generate()
+from .pages.family_pages import page_template as family_page_template
+
+birthday_frame = master.single_frame(doc, id='normal')
+
 from . import section_ids
 
 # setting master
-doc.addPageTemplates([PageTemplate(id=section_ids.dual_row_family_id, frames=families_info_page_frame, onPage=master.default_master),
-                    #   PageTemplate(id=message_section_id, frames=single_frame, onPage=message_master),
-                      ])
+doc.addPageTemplates([
+  family_page_template,
+  PageTemplate(id=section_ids.birthday_pages_id, frames=birthday_frame, onPage=master.default_master),
+  ])
 
 # Adding elements
 from .contents import Elements
