658d00c881fd9c3d5908b3e04d0c9677678ca0be
Santo K Thomas <49450970+santokalayil@users.noreply.github.com>
2021-04-01 13:37:30 +0530

    committing files

---
+++ directory/pages/birthdays/__init__.py
@@ -0,0 +1,36 @@
+section_id = 'Birthdays'
+
+from directory import master
+from directory import doc
+page_object = master.single_frame(doc, section_id)
+# (doc, ["box_top", "box_bottom"])
+# page_frame = page_object.generate()
+page_frame = page_object
+
+from reportlab.platypus import PageTemplate
+# from directory import section_ids
+page_template = PageTemplate(
+        id=section_id, 
+        frames=page_frame,
+        onPage=master.default_master,
+    )
+
+
+# from .family_box import generate_box_elements
+from directory.database.get import basic_query
+from directory.settings import db_url
+
+def generate():
+    Elements = []
+    from reportlab.platypus import NextPageTemplate
+    Elements.append(NextPageTemplate(section_id))
+    from reportlab.platypus import FrameBreak
+    Elements.append(FrameBreak())
+    # sql = basic_query(db_url).sql_dataframe
+    # order_of_families = sql(
+    #     '''select * from members where rltshp = "Self" order by member_name'''
+    #     ).famid.to_dict().values()
+
+    # for order_id, fam_id in enumerate(order_of_families, start=1):
+    #     Elements += generate_box_elements(fam_id, order_id)
+    return Elements
