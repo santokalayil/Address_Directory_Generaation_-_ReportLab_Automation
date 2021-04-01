272d9ecc71c4b56532f23833527852781527c0f1
Santo K Thomas <49450970+santokalayil@users.noreply.github.com>
2021-04-01 11:58:28 +0530

    committing files

--- directory/contents.py
+++ directory/contents.py
@@ -3,7 +3,7 @@ from .settings import PAGE_FRAME_WIDTH, PAGE_FRAME_HEIGHT
 from .settings import db_url
 
 from reportlab.lib.styles import ParagraphStyle
-from reportlab.platypus import Paragraph, NextPageTemplate, FrameBreak
+from reportlab.platypus import Paragraph, NextPageTemplate, FrameBreak, PageBreak, PageBreakIfNotEmpty
 
 
 
@@ -12,14 +12,8 @@ Elements = []
 Elements.append(NextPageTemplate(section_ids.dual_row_family_id))  # marking section id to identify master page and style
 
 
+from .pages.family_pages import generate as generate_family_pages
+Elements += generate_family_pages()
 
-# going to each family data and constructing elements
-from .pages.family_pages.family_box import generate_box_elements
-from .database.get import basic_query
-sql = basic_query(db_url).sql_dataframe
-order_of_families = sql(
-    '''select * from members where rltshp = "Self" order by member_name'''
-    ).famid.to_dict().values()
-
-for order_id, fam_id in enumerate(order_of_families, start=1):
-    Elements += generate_box_elements(fam_id, order_id)
+Elements.append(PageBreakIfNotEmpty())
+Elements.append(NextPageTemplate(section_ids.birthday_pages_id))
