b2ca3a4618d85ae61a709607291e49260fff4d5e
Santo K Thomas <49450970+santokalayil@users.noreply.github.com>
2021-04-04 00:03:05 +0530

    committing files

--- directory/contents.py
+++ directory/contents.py
@@ -5,15 +5,13 @@ from .settings import db_url
 from reportlab.lib.styles import ParagraphStyle
 from reportlab.platypus import Paragraph, NextPageTemplate, FrameBreak, PageBreak, PageBreakIfNotEmpty
 
-
 Elements = []
-
 
 from .pages.family_pages import generate as generate_family_pages
 Elements += generate_family_pages()
 
-Elements.append(PageBreakIfNotEmpty())
-
-
 from .pages.birthdays import generate as generate_birthday_pages
 Elements += generate_birthday_pages()
+
+from .pages.anniversaries import generate as generate_anniversary_pages
+Elements += generate_anniversary_pages()
