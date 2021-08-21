e4d6c7e71e9aeb80fe9aba12b79716e48db79858
Santo K Thomas <49450970+santokalayil@users.noreply.github.com>
2021-08-21 23:47:00 +0530

    committing files

--- directory/pages/family_pages/family_box.py
+++ directory/pages/family_pages/family_box.py
@@ -93,7 +93,7 @@ def address_section(common_data):
     address_paragraph = Paragraph(
         f"""Residential Address: {common_data['text']['current_address']}"""+new_line+
         f"""Native Address: {common_data['text']['native_address']}"""+new_line+
-        f"""Native Parish:{common_data['text']['native_parish']}"""+new_line+  # common_data['text']['diocese'] , # remove comma if using next line
+        f"""Native Parish: {common_data['text']['native_parish']}"""+new_line+  # common_data['text']['diocese'] , # remove comma if using next line
         f"""Diocese: {common_data['text']['diocese']}""", 
         styleN
         )
